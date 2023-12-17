d2178a8ee3b34641fea9e73bae3e78243e7c07c1
joejaker <116920060+joejaker@users.noreply.github.com>
2023-12-17 17:44:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { createBareServer } from '@tomphttp/bare-server-node';
 import express from "express";
 import { createServer } from "node:http";
-import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { uvPath } from "@joejaker/cool";
 import { join } from "node:path";
 import { hostname } from "node:os";
 import { fileURLToPath } from "url";
